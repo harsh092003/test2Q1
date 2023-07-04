d8181b488fe0be87750ae8ec3d8ebf27ed695b3a
Harsh Anand <118230039+harsh092003@users.noreply.github.com>
2023-07-04 23:44:42 +0530

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,17 @@
+
+    public class test1 {
+
+    public static void main(String args[]) {
+            int number = 78;
+            System.out.print("your number is "+ number +" and ");
+            
+            if (number > 0) {
+                System.out.println("The number you enterd is positive.");
+            } else if (number < 0) {
+                System.out.println("The number you entered is negative.");
+            } else {
+                System.out.println("The number you entered is zero.");
+            }
+        }
+    }
+
